5034be3e2751cbaea8355a46f300011c030ceb71
dyushakolesov <dyushakolesov@mail.ru>
2020-12-07 15:57:59 +0300

    average running time of algorithm

--- voronoi_main.cpp
+++ voronoi_main.cpp
@@ -28,36 +28,45 @@ int main(int argc, char **argv)
 	srand(time(NULL));
 	float *xValues = new float[count];
 	float *yValues = new float[count];
+	float avg = 0;
+	int end_time;
+	int search_time;
+	unsigned int start_time;
+		for (int j = 0; j < 100; j++) {
 
-	//generate random elements from -1000 to 1000
-	for (int i = 0; i < count; i++) {
-		xValues[i] = rand()%2001 - 1000;
-		yValues[i] = rand()%2001 - 1000;
-	}
+			//generate random elements from -1000 to 1000
+			for (int i = 0; i < count; i++) {
+				xValues[i] = rand() % 2001 - 1000;
+				yValues[i] = rand() % 2001 - 1000;
+			}
+
+
+			VoronoiDiagramGenerator vdg;
+
+			start_time = clock(); // start time
+
+			vdg.generateVoronoi(xValues, yValues, count, -1000, 1000, -1000, 1000, 3);
+
+			end_time = clock(); // end time
+			search_time = end_time - start_time; // result time
+			avg += search_time;
+		}
+		avg /= 100;
+	/* optut for all lines if you want to check algorithm
 	
-
-	VoronoiDiagramGenerator vdg;
-
-	unsigned int start_time = clock(); // start time
-
-	vdg.generateVoronoi(xValues,yValues, count, -1000,1000,-1000,1000,3);
-	
-	int end_time = clock(); // end time
-	int search_time = end_time - start_time; // result time
-
-
-	vdg.resetIterator();
 
 	float x1,y1,x2,y2;
 
 	
 	printf("\n-------------------------------\n");
+	
 	while(vdg.getNext(x1,y1,x2,y2))
 	{
 		printf("GOT Line (%f,%f)->(%f,%f)\n",x1,y1,x2, y2);
 		
 	}
-	printf("result time %d \n", search_time);
+	*/
+	printf("result time %f \n", avg);
 
 	return 0;
 
